97bad45154aa54328a79d04131f1e64cf9647648
abdullah <abdullahturkak@gmail.com>
2023-01-05 09:39:46 +0300

    commit

--- lib/Screen/ProgramScreen.dart
+++ lib/Screen/ProgramScreen.dart
@@ -16,7 +16,7 @@ class _ProgramScreenState extends State<ProgramScreen> {
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
-          children: <Widget>[
+          children:[
             Container(
               child: Center(
                 child: Text(
@@ -31,6 +31,84 @@ class _ProgramScreenState extends State<ProgramScreen> {
                       image: AssetImage('assets/btnfoto11.jpeg'),
                       fit: BoxFit.fitHeight)),
             ),
+            Container(
+                width: MediaQuery.of(context).size.width,
+                height: 400,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 100,
+                      child: Container(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Antrenman 1'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blueGrey),
+                            overlayColor: MaterialStateProperty.all(Colors.red),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 100,
+                      child: Container(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Antrenman 2'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blueGrey),
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.yellow),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 100,
+                      child: Container(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Antrenman 3'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blueGrey),
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 100,
+                      child: Container(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('Antrenman 3'),
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.blueGrey),
+                            overlayColor:
+                                MaterialStateProperty.all(Colors.blue),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
           ],
         ),
       ),
