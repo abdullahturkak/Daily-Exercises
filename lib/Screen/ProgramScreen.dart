c4e829c8957317d104f99eb263631a5f1c7c80db
abdullah <abdullahturkak@gmail.com>
2023-01-05 12:17:12 +0300

    commit

--- lib/Screen/ProgramScreen.dart
+++ lib/Screen/ProgramScreen.dart
@@ -13,105 +13,111 @@ class _ProgramScreenState extends State<ProgramScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children:[
-            Container(
-              child: Center(
-                child: Text(
-                  'This is My Text',
-                  style: TextStyle(color: Colors.white, fontSize: 30),
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child:Expanded(
+          child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          reverse: true,
+          child: Column(
+            children:[
+              Container(
+                child: Center(
+                  child: Text(
+                    'This is My Text',
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
                 ),
+                height: 300,
+                width: MediaQuery.of(context).size.width - 5,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/btnfoto11.jpeg'),
+                        fit: BoxFit.fitHeight)),
               ),
-              height: 300,
-              width: MediaQuery.of(context).size.width - 5,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/btnfoto11.jpeg'),
-                      fit: BoxFit.fitHeight)),
-            ),
-            Container(
-                width: MediaQuery.of(context).size.width,
-                height: 400,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 10,
-                      height: 100,
-                      child: Container(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Antrenman 1'),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueGrey),
-                            overlayColor: MaterialStateProperty.all(Colors.red),
+              Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 400,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 10,
+                        height: 100,
+                        child: Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Antrenman 1'),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blueGrey),
+                              overlayColor: MaterialStateProperty.all(Colors.red),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 10,
-                      height: 100,
-                      child: Container(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Antrenman 2'),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueGrey),
-                            overlayColor:
-                                MaterialStateProperty.all(Colors.yellow),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 10,
+                        height: 100,
+                        child: Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Antrenman 2'),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blueGrey),
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.yellow),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 10,
-                      height: 100,
-                      child: Container(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Antrenman 3'),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueGrey),
-                            overlayColor:
-                                MaterialStateProperty.all(Colors.blue),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 10,
+                        height: 100,
+                        child: Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Antrenman 3'),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blueGrey),
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.blue),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 10,
-                      height: 100,
-                      child: Container(
-                        margin: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Antrenman 3'),
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueGrey),
-                            overlayColor:
-                                MaterialStateProperty.all(Colors.blue),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 10,
+                        height: 100,
+                        child: Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Antrenman 3'),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blueGrey),
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.blue),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+            ],
+          ),
               ),
-          ],
         ),
-      ),
+      )
     );
   }
 }
